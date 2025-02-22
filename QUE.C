3c2ad5c895bd7289dba8bb03d217af3860e01ab1
Vetriselvan <vvetriselvan301@gmail.com>
2025-02-22 10:51:43 +0530

    Update QUE.C

--- QUE.C
+++ QUE.C
@@ -1,30 +1,90 @@
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<stdbool.h>
 # define MAX 5
-int que[MAX];
-int rear = -1;
-int front = -1;
+int q[MAX];
+int r = -1;
+int f = -1;
 int x,value;
-int enqueue(int q,int rear)
+int isfull()
+ {
+     if(r==MAX-1)
+     return 1;
+     else
+     return 0;
+ }
+ int isempty()
+ {
+     if(f==-1)
+     return 1;
+     else
+     return 0;
+ }
+int enqueue(int q,int r)
 {
-    if(rear == -1)
-        rear = front = 0;
+    if(isfull())
+    printf("Q full\n");
     else
-        rear ++;
-    q[rear] = x;
+        r = f = 0;
+        r ++;
+        q[r] = x;
+return x;
 }
  int dequeue(int q)
  {
-    // value = q[front];
-     front++;
-     return value;
+     if (isempty())
+          printf("Q is empty\n");
+     else
+       {
+          if(r==f)
+           r=-1,f=-1;
+           value = q[f];
+       
+     else
+        f++;
+       }
+return value;
  }
+ 
 int main()
 {
-    int q;
-    int rear = -1;
-    q = 12,33,45,66;
-    printf("the push %d\n",enqueue(q),rear);
-    printf("the pop %d\n",dequeue(q));
+  int choice = 0,val;
+  int a,b;
+  while(choice!=5)
+  {
+      printf("Enter the choice\n");
+      printf("1.Enqueue\n");
+      printf("2.Dequeue\n");
+      printf("3.Check Full\n");
+      printf("4.Check Empty\n");
+      printf("5.Exit\n");
+      scanf("%d",&choice);
+      switch(choice)
+      {
+        case 1:
+          printf("Enter the Enqueue elements: \n");
+          scanf("%d\n",&val);
+          a = enqueue(val,r);
+          printf("%d\n",a);
+          break;
+        case 2:
+           b = dequeue();
+           printf("%d\n",b);
+        case 3:
+          if(isfull())
+          printf("The linear Q is full \n");
+          else
+          printf("The linear Q is not full \n");
+          break;
+        case 4:
+          if(isempty())
+          printf("The linear Q is empty \n");
+          else
+          printf("The linear Q is not empty \n");
+          break;
+      }
+      
+  }
+  return 0;
 }
